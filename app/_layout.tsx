4767d1d88925296df701dd26593d49558c032351
nabilamatta <118593936+nabilamatta@users.noreply.github.com>
2025-07-19 00:46:40 +0800

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,18 +3,16 @@ import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
-// Tahan splash screen hingga semua aset siap dimuat
 SplashScreen.preventAutoHideAsync();
 
-// Daftar font yang ingin digunakan dalam aplikasi
-const customFontList = {
-  // 5 Font Statis
+const daftarFontKustom = {
+  // Font Statis
   'Roboto-Regular': require('../assets/fonts/static/Roboto-Regular.ttf'),
   'Lato-Bold': require('../assets/fonts/static/Lato-Bold.ttf'),
   'Oswald-Regular': require('../assets/fonts/static/Oswald-Regular.ttf'),
   'Raleway-Bold': require('../assets/fonts/static/Raleway-Bold.ttf'),
   'Montserrat-Regular': require('../assets/fonts/static/Montserrat-Regular.ttf'),
-  // 5 Font Variabel
+  // Font Variabel
   'Inter-Variable': require('../assets/fonts/variable/Inter-VariableFont_opsz,wght.ttf'),
   'WorkSans-Variable': require('../assets/fonts/variable/WorkSans-VariableFont_wght.ttf'),
   'Manrope-Variable': require('../assets/fonts/variable/Manrope-VariableFont_wght.ttf'),
@@ -22,25 +20,22 @@ const customFontList = {
   'Outfit-Variable': require('../assets/fonts/variable/Outfit-VariableFont_wght.ttf'),
 };
 
-export default function LayoutUtama() {
-  const [fontSiap, kesalahanFont] = useFonts(customFontList);
+export default function TataLetakAplikasi() {
+  const [sudahSiapFont, gagalMuatFont] = useFonts(daftarFontKustom);
 
   useEffect(() => {
-    // Sembunyikan splash screen jika font sudah dimuat atau ada error
-    if (fontSiap || kesalahanFont) {
+    if (sudahSiapFont || gagalMuatFont) {
       SplashScreen.hideAsync();
     }
-  }, [fontSiap, kesalahanFont]);
+  }, [sudahSiapFont, gagalMuatFont]);
 
-  // Jika font belum dimuat dan tidak ada error, tampilkan splash screen saja
-  if (!fontSiap && !kesalahanFont) {
+  if (!sudahSiapFont && !gagalMuatFont) {
     return null;
   }
 
-  // Render navigasi utama setelah font tersedia
   return (
     <Stack>
-      <Stack.Screen name="tugas4" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
