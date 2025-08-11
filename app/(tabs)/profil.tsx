b83f055fda4e1805ec9a6d04718572a6de4eb806
nabilamatta <118593936+nabilamatta@users.noreply.github.com>
2025-08-11 22:18:37 +0800

    Update profil.tsx

--- app/(tabs)/profil.tsx
+++ app/(tabs)/profil.tsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import { Text, View, Image, ScrollView } from 'react-native';
-import { styles } from '../styles'; // Impor gaya dari file terpisah
+import { styles } from '../styles'; // Mengimpor style dari file terpisah
 
-export default function ProfilScreen() {
+export default function DataProfilScreen() {
   return (
-    <ScrollView style={styles.container}>
-        <View style={styles.content}>
-            <Image
-                source={require('../../assets/images/foto-profil.png')}
-                style={styles.profileImage}
-            />
-            <Text style={styles.profileName}>NURMISBA</Text>
+    <ScrollView style={styles.mainWrapper}>
+      <View style={styles.innerContent}>
+        <Image
+          source={require('../../assets/images/gambar1.jpeg')}
+          style={styles.avatarImage}
+        />
+        <Text style={styles.displayName}>NABILA ISMAIL MATTA</Text>
 
-            <View style={styles.profileInfoContainer}>
-                <View style={styles.infoRow}>
-                    <Text style={styles.infoLabel}>NIM</Text>
-                    <Text style={styles.infoValue}>105841103422</Text>
-                </View>
-                <View style={styles.infoRow}>
-                    <Text style={styles.infoLabel}>Kelas</Text>
-                    <Text style={styles.infoValue}>TI-6A</Text>
-                </View>
-                <View style={styles.infoRow}>
-                    <Text style={styles.infoLabel}>Jurusan</Text>
-                    <Text style={styles.infoValue}>Teknik Informatika</Text>
-                </View>
-                <View style={styles.infoRow}>
-                    <Text style={styles.infoLabel}>Fakultas</Text>
-                    <Text style={styles.infoValue}>Teknik</Text>
-                </View>
-            </View>
+        <View style={styles.detailsSection}>
+          <View style={styles.detailRow}>
+            <Text style={styles.detailLabel}>NIM</Text>
+            <Text style={styles.detailValue}>105841100722</Text>
+          </View>
+          <View style={styles.detailRow}>
+            <Text style={styles.detailLabel}>Kelas</Text>
+            <Text style={styles.detailValue}>TI-6A</Text>
+          </View>
+          <View style={styles.detailRow}>
+            <Text style={styles.detailLabel}>Jurusan</Text>
+            <Text style={styles.detailValue}>Teknik Informatika</Text>
+          </View>
+          <View style={styles.detailRow}>
+            <Text style={styles.detailLabel}>Fakultas</Text>
+            <Text style={styles.detailValue}>Teknik</Text>
+          </View>
         </View>
+      </View>
     </ScrollView>
   );
-}       
+}
